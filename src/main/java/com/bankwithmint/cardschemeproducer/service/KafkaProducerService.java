7f397f899b7948b19f7b75bb09aa332ce28e98d4
Franklyn Ogbonna <franklynogbonna@Franklyns-MacBook-Pro.local>
2020-06-21 03:17:07 +0100

    feat: implement kafka producer service

---
+++ src/main/java/com/bankwithmint/cardschemeproducer/service/KafkaProducerService.java
@@ -0,0 +1,40 @@
+package com.bankwithmint.cardschemeproducer.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class KafkaProducerService {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaProducerService.class);
+    private ExecutorService executorService;
+
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic, String message) {
+        logger.info(String.format("[+] ===> Producing message ==> %s", message));
+        this.kafkaTemplate.send(topic, message);
+    }
+
+    @PostConstruct
+    private void create() {
+        executorService = Executors.newSingleThreadExecutor();
+    }
+
+    public void process(Runnable operation) {
+        // submit runnable operation
+        executorService.submit(operation);
+    }
+
+    @PreDestroy
+    private void destroy() {
+        executorService.shutdown();
+    }
+}
