dde15568b9796531a2f041fb614f56b71e07c605
Franklyn Ogbonna <franklynogbonna@Franklyns-MacBook-Pro.local>
2020-06-21 02:16:28 +0100

    initial commit

---
+++ src/main/java/com/bankwithmint/cardschemeproducer/CardSchemeProducerApplication.java
@@ -0,0 +1,13 @@
+package com.bankwithmint.cardschemeproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardSchemeProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardSchemeProducerApplication.class, args);
+    }
+
+}
