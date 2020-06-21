b5d30e2d9b9c0fa54d6fca0cb7ec563f4aa547e6
Franklyn Ogbonna <franklynogbonna@Franklyns-MacBook-Pro.local>
2020-06-21 02:36:27 +0100

    feat: add kafka producer configuration class

--- src/main/java/com/bankwithmint/cardschemeproducer/config/AppConfig.java
+++ src/main/java/com/bankwithmint/cardschemeproducer/config/AppConfig.java
@@ -1,12 +1,10 @@
 package com.bankwithmint.cardschemeproducer.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
---
+++ src/main/java/com/bankwithmint/cardschemeproducer/config/KafkaProducerConfig.java
@@ -0,0 +1,38 @@
+package com.bankwithmint.cardschemeproducer.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KafkaProducerConfig {
+    @Autowired
+    AppConfig appConfig;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                appConfig.getKafkaBootstrapServers());
+        configProps.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        configProps.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        configProps.put(ProducerConfig.RECONNECT_BACKOFF_MS_CONFIG, "10000");
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
